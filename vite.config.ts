b85acab3815b50e8252eab9013f1f0dda3570cc1
Abdelkarim Moussi <abdelkarimmoussi73@gmail.com>
2026-01-29 16:04:07 +0000

    feat: add CORS proxy configuration for API requests

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailiwindcss from '@tailwindcss/vite'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailiwindcss()],
-
+  plugins: [react(), tailwindcss()],
+  server: {
+    port: 5173,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8080',
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
 })
